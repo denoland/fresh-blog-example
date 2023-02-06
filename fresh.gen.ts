7e5a5c3c6bf0e4578609d9aa3d1e9c0c8748ff58
Taylor Y <tdy721@gmail.com>
2023-02-06 11:25:41 -0700

    add 404 route some error handling

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,12 +4,14 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/[slug].tsx";
-import * as $1 from "./routes/index.tsx";
+import * as $1 from "./routes/_404.tsx";
+import * as $2 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/[slug].tsx": $0,
-    "./routes/index.tsx": $1,
+    "./routes/_404.tsx": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {},
   baseUrl: import.meta.url,
